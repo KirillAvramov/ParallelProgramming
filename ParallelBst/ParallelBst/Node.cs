82e0490c2b8dde05e8b4cd54630127cece44b0d4
KirillAvramov <woks1234@gmail.com>
2018-01-09 15:50:36 +0300

    add Node.cs via upload

---
+++ ParallelBst/ParallelBst/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ParallelBst
+{
+    public class Node<TKey> where TKey : IComparable<TKey>
+    {
+        
+        public TKey Key { get; set; }
+            
+        public Node(TKey k)
+        {
+            Key = k;
+        }
+
+        public Node<TKey> Father = null;
+        public Node<TKey> LeftChild = null;
+        public Node<TKey> RightChild = null;
+
+        
+    }
+}
